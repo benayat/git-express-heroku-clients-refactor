3826220e5a0ab874f155b51c0e26ea67d1f8dea0
Benaya Trabelsi <benaya7@gmail.com>
2021-04-18 14:57:01 +0300

    again

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const router = require('./router/router');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build', 'index.html'));
+  });
+}
 
 app.use(express.json());
 app.use(router);
