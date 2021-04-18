70b3e439d4ed3cf857a8f44a534efcede516e874
Benaya Trabelsi <benaya7@gmail.com>
2021-04-18 15:21:49 +0300

    again

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './build')));
   app.get('*', (req, res) => {
