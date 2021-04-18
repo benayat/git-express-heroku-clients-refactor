683a79cccfa37969e4b8d8f4b5b72a08f4c137a0
Benaya Trabelsi <benaya7@gmail.com>
2021-04-18 15:15:44 +0300

    again

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, './build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './build', 'index.html'));
   });
