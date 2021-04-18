81a067caba953227484803910996df0e01fbba95
Benaya Trabelsi <benaya7@gmail.com>
2021-04-18 15:26:23 +0300

    again

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(cors());
 // app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, './build', 'index.html'));
+  // });
 }
 
 app.use(express.json());
